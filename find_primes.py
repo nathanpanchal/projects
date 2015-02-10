76dd013ba06fb14643b66b5b15f161e15b798383
Nathan Panchal <nathanpanchal@gmail.com>
2015-02-10 14:20:16 -0800

    initial upload - np

---
+++ find_primes.py
@@ -0,0 +1,17 @@
+# the following code finds all the prime numbers between two values. Note this code excludes end value
+
+def is_prime(n):
+    if n == 0 or n == 1:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    else:
+        return True
+
+begining = int(raw_input("Begining value: "))
+end = int(raw_input("End value: "))
+
+for n in range(begining, end):
+    if is_prime(n):
+        print n
