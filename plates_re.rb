180aa8090116b8c63951dc74f86725ac188e7dea
Nathan Panchal <nathanpanchal@gmail.com>
2015-03-01 09:52:10 -0800

    initial upload - np

---
+++ plates_re.rb
@@ -0,0 +1,32 @@
+# This is a refactored version of plates.rb
+
+print "What is your desired weight: "
+weight = gets.chomp.to_f
+
+# Assumes the barbell being used is 45 lbs.
+n = (weight - 45) / 2
+
+# Weight plates are in lbs
+num_fourtyfive = 0
+num_thirtyfive = 0
+num_twentyfive = 0
+num_ten = 0
+num_five = 0
+num_twohalf = 0
+
+num_fourtyfive = n / 45
+num_thirtyfive = (n % 45) / 35
+num_twentyfive = ((n % 45) % 35) / 25
+num_ten = (((n % 45) % 35) % 25) / 10
+num_five = ((((n % 45) % 35) % 25) % 10) / 5
+num_twohalf = (((((n % 45) % 35) % 25) % 10) % 5) / 2.5
+
+puts """
+You need the following:
+\t#{num_fourtyfive} fourty fives
+\t#{num_thirtyfive} thirty fives
+\t#{num_twentyfive} twenty fives
+\t#{num_ten} tens
+\t#{num_five} fives
+\t#{num_twohalf} two and a halfs
+"""
