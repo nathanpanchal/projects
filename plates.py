67f6f5313a8f83274b941bc3aaea17432f83d7fe
Nathan Panchal <nathanpanchal@gmail.com>
2015-03-01 09:16:12 -0800

    sync error - np
    the sync erased the original code and replaced it with code with the
    same file name - np

--- plates.py
+++ plates.py
@@ -1,21 +1,40 @@
-# Code computes what plates are needed to achive a given weight. This code does not work perfectly
-# because it only accepts int values and prints ALL possible combinations of plates.
-
-weight = int(raw_input("What is your desired weight: "))
+weight = float(raw_input("What is your desired weight: "))
 n = (weight - 45) / 2
 
-first = 2
-second = 5
-third = 10
-fourth = 25
-fifth = 35
-sixth = 45
+num_fourtyfive = 0
+num_thirtyfive = 0
+num_twentyfive = 0
+num_ten = 0
+num_five = 0
+num_twohalf = 0
 
-for a in range(0, (n/first)+1):
-	for b in range(0, (n/second)+1):
-		for c in range(0, (n/third)+1):
-			for d in range(0, (n/fourth)+1):
-				for e in range(0, (n/fifth)+1):
-					for f in range(0, (n/sixth)+1):
-						if a*first + b*second + c*third + d*fourth + e*fifth + f*sixth == n:
-							print (a, b, c, d, e, f)
+remaining = n
+if n < 2.5:
+	print "Use the bar only"
+else:
+ 	while remaining >= 45:
+		remaining = remaining - 45
+		num_fourtyfive += 1
+	while remaining >= 35:
+		remaining = remaining - 35
+		num_thirtyfive += 1
+	while remaining >= 25:
+		remaining = remaining - 25
+		num_twentyfive += 1
+	while remaining >= 10:
+		remaining = remaining - 10
+		num_ten += 1
+	while remaining >= 5:
+		remaining = remaining - 5
+		num_five += 1
+	while remaining >= 2.5:
+		remaining = remaining - 2.5
+		num_twohalf += 1
+	print """You need:
+	%d fourty fives
+	%d thirty fives
+	%d twenty fives
+	%d tens
+	%d fives
+	%d two and a halfs
+	""" % (num_fourtyfive, num_thirtyfive, num_twentyfive, num_ten, num_five, num_twohalf)
