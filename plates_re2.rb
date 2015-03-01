2bd9c8fe9a2551cf7f29f281b4279cbb5ee6ba92
Nathan Panchal <nathanpanchal@gmail.com>
2015-03-01 12:47:50 -0800

    initial upload - np

---
+++ plates_re2.rb
@@ -0,0 +1,14 @@
+weights = [45, 35, 25, 10, 5, 2.5]
+
+def plates(weight, divisors)
+	plates = []
+	remaining = weight
+	for n in divisors
+		# Can not refer to weight in the loop because we will always be dividing by the full amount
+		# for each iteration resulting in inaccurate nums.
+		num = remaining / n
+		remaining = remaining % n
+		plates << num.to_i
+	end
+	print plates
+end
