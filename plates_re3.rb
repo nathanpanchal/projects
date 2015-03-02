174b9ceb50bf3104d2500388f1442d514c913632
Nathan Panchal <nathanpanchal@gmail.com>
2015-03-01 16:11:21 -0800

    changed to function only - np

--- plates_re3.rb
+++ plates_re3.rb
@@ -1,6 +1,5 @@
 imperial_plates = [45, 35, 25, 10, 5, 2.5]
 
-
 def plates(weight, plates)
 	side_weight = (weight - 45) / 2
 	remaining = side_weight
@@ -10,13 +9,3 @@ def plates(weight, plates)
 		num.to_i
 	end.inspect
 end
-
-
-print "What is your max? "
-weight = gets.chomp.to_f
-
-puts plates(weight * 0.6, imperial_plates)
-puts plates(weight * 0.65, imperial_plates)
-puts plates(weight * 0.7, imperial_plates)
-puts plates(weight * 0.75, imperial_plates)
-puts plates(weight * 0.8, imperial_plates)
