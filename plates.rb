40e15ef37a3b80ee61107c27c2c12344d1330343
Nathan Panchal <nathanpanchal@gmail.com>
2015-03-02 20:52:01 -0800

    np - refactor uses .map to iterate over the array
    this refactor uses one less line of code

--- plates.rb
+++ plates.rb
@@ -1,17 +1,14 @@
 # Takes a desired weight and prints what plates need to be placed on each side of a barbel to
 # achive that weight.
 
-weights = [45, 35, 25, 10, 5, 2.5]
+imperial_plates = [45, 35, 25, 10, 5, 2.5]
 
-def plates(weight, divisors)
-	plates = []
-	remaining = weight
-	for n in divisors
-		# Can not refer to weight in the loop because we will always be dividing by the full amount
-		# for each iteration resulting in inaccurate nums.
-		num = remaining / n
-		remaining = remaining % n
-		plates << num.to_i
-	end
-	return plates
+def plates(weight, plates)
+	side_weight = (weight - 45) / 2
+	remaining = side_weight
+	plates.map do |plate|
+		num = remaining / plate
+		remaining = remaining % plate
+		num.to_i
+	end.inspect
 end
