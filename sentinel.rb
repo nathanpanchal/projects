b4d3985ad509af401fff9e2747802a527b2d3f62
Nathan Panchal <nathanpanchal@gmail.com>
2015-03-23 20:21:27 -0700

    np - add while loop method
    Add a while loop method that checks for the correct input. If the user
    does not give the correct input it repeats the correct input is given.

--- sentinel.rb
+++ sentinel.rb
@@ -1,10 +1,25 @@
-run_again = 'y'
+@first_run = true
 
-begin
+def run_again?
+	if @first_run
+		@first_run = false
+		# exits the method and returns true
+		return true
+	end
+	# initialize play again variable
+	run_again = ''
+	# allows user to set which inputs are acceptable by adding to the array
+	while ! ['n', 'y'].include?(run_again)
+		puts "Would you like to run again? (y/n)"
+		print "> "
+		run_again = gets.chomp!
+	end
+	# exits the method and returns true if the expression (run_again == 'y') evaluates to true
+	return (run_again == 'y')
+end
+
+while run_again?
 
 	puts "running program..."
 
-	puts "would you like to run again? (y/n)"
-	run_again = gets.chomp!
-
-end while run_again == 'y'
+end
