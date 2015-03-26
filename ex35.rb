544d79ea117860d66633eeb416bcd734c0cdd2bf
Nathan Panchal <nathanpanchal@gmail.com>
2015-03-25 19:15:47 -0700

    np - remove double quotes, add choice.include

--- ex35.rb
+++ ex35.rb
@@ -1,63 +1,63 @@
 # Exercise 35: Branches and Functions
 
 def gold_room
-  puts "This room is full of gold. How much do you take?"
+  puts 'This room is full of gold. How much do you take?'
 
-  print "> "
+  print '> '
   choice = gets.chomp
 
   #this line has a bug, so fix it
-  if choice.include?("0") || choice.include?("1")
+  if choice.include?('1') || choice.include?('1')
     how_much = choice.to_i
   else
-    puts "Man learn to type a number."
+    puts 'Man learn to type a number.'
   end
 
   if how_much < 50
-    puts "Nice, you're not greedy, you win!"
+    puts 'Nice, you\'re not greedy, you win!'
     exit(0)
   else
-    dead("You greedy bastard!")
+    dead('You greedy bastard!')
   end
 end
 
 def bear_room
-  puts "There is a bear here."
-  puts "The bear has a bunch of honey."
-  puts "The fat bear is in front of another door."
-  puts "How are you going to move the bear?"
+  puts 'There is a bear here.'
+  puts 'The bear has a bunch of honey.'
+  puts 'The fat bear is in front of another door.'
+  puts 'How are you going to move the bear?'
 
   bear_moved = false
 
   while true
-    print "> "
+    print '> '
     choice = gets.chomp
 
-    if choice == 'take honey'
-      dead('The bear looks at you then slaps your face off')
-    elsif choice == 'taunt bear' && !bear_moved
-      puts "The bear has moved from the door. You can go through it now."
+    if choice.include?('honey')
+      dead('Why would you mess with his food? The bear looks at you then slaps your face off')
+    elsif choice.include?('taunt') && !bear_moved
+      puts 'The bear has moved from the door. You can go through it now.'
       bear_moved = true
-    elsif choice == "taunt bear" && bear_moved
-      dead("The bear gets pissed off and chews your leg off.")
-    elsif choice == "open door" && bear_moved
+    elsif choice.include?('taunt') && bear_moved
+      dead('The bear gets pissed off and chews your leg off.')
+    elsif choice.include?('open') && bear_moved
       gold_room
     else
-      puts "I got no idea what that means"
+      puts 'I got no idea what that means'
     end
   end
 end
 
 def cthulhu_room
-  puts "Here you see the great evil Cthulhu"
-  puts "He...it...whatever stares at you and you go insane."
-  puts "So you flee for your life or eat your head?"
+  puts 'Here you see the great evil Cthulhu'
+  puts 'He...it...whatever stares at you and you go insane.'
+  puts 'So you flee for your life or eat your head?'
 
-  print "> "
+  print '> '
   choice = gets.chomp
 
-  if choice.include? "head"
-    dead("You saw that coming...I bet it wasn't tasty")
+  if choice.include? 'head'
+    dead('You saw that coming...I bet it wasn\'t tasty')
   elsif chice.include? 'flee'
     start
   else
@@ -66,7 +66,7 @@ def cthulhu_room
 end
 
 def dead(why)
-  puts why, "Good job!"
+  puts why, 'Good job!'
   exit(0)
 end
 
