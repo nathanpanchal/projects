5a2676171150c817af7ad0d35c24b0e623aed74e
Nathan Panchal <nathanpanchal@gmail.com>
2015-04-05 21:42:25 -0700

    np - initial commit

---
+++ RockPaperScissors.rb
@@ -0,0 +1,41 @@
+puts "Do you choose rock, paper, or scissors?"
+print '> '
+user_choice = gets.chomp
+
+def computer_choice()
+  computer_choice = rand
+  if computer_choice < 0.33
+    computer_choice = 'rock'
+  elsif computer_choice <= 0.67
+    computer_choice = 'paper'
+  else
+    computer_choice = 'scissors'
+  end
+  puts "Computer: #{computer_choice}"
+end
+
+def compare(choice1, choice2)
+  if choice1 == choice2
+    puts "The result is a tie!"
+  elsif choice1 == 'rock'
+    if choice2 == 'scissors'
+      puts 'rock wins!'
+    else
+      puts 'paper wins!'
+    end
+  elsif choice1 == 'paper'
+    if choice2 == 'rock'
+      puts 'paper wins!'
+    else
+      puts 'scissors wins!'
+    end
+  else
+    if choice2 == 'paper'
+      puts 'scissors wins!'
+    else
+      puts 'rock wins!'
+    end
+  end
+end
+
+compare(user_choice, computer_choice)
